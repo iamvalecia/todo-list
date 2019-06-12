5562f361b07e44b288ddc170b31fe49f3c29d0ac
Valecia Briars <vbriars@hotmail.com>
2019-06-12 15:58:02 -0500

    finished editTodo() and editComplete()

--- todo.js
+++ todo.js
@@ -1,58 +1,68 @@
 
-    var todoUl = document.getElementById('todoUl')
-    var newTodo = document.getElementById('newTodo');
-    // var todoEditInput = document.getElementById('')
+var todoUl = document.getElementById('todoUl');
+var newTodo = document.getElementById('newTodo');
+    
 function addItem() {
     todoLi = document.createElement('li');
-    todoSpan = document.createElement('span')
-    todoSpan.style.textDecoration = 'none'
-    todoEditInput = document.createElement('input')
-    todoEditInput.style.display = 'none'
-    
+    todoSpan = document.createElement('span');
+    todoSpan.style.textDecoration = 'none';
+    todoEditInput = document.createElement('input');
+    todoEditInput.style.display = 'none';
     todoSpan.innerHTML = newTodo.value;
-    todoSpan.setAttribute('onclick', 'strikeOut(this)')
+    todoSpan.setAttribute('onclick', 'strikeOut(this)');
     var editButton = document.createElement('button');
     editButton.textContent = "Edit";
-    editButton.setAttribute('onclick', 'editTodo(this)')
+    editButton.setAttribute('onclick', 'editTodo(this)');
     var deleteButton = document.createElement('button');
     deleteButton.textContent = 'Delete';
     deleteButton.className = 'deleteButton';
-    deleteButton.setAttribute('onclick', 'deleteTodo(this)')
-    todoUl.appendChild(todoLi)
-    todoLi.appendChild(todoEditInput)
-    todoLi.appendChild(todoSpan)
-    todoLi.appendChild(editButton)
-    todoLi.appendChild(deleteButton)
-    newTodo.value = ""
-    saveList()
+    deleteButton.setAttribute('onclick', 'deleteTodo(this)');
+    todoUl.appendChild(todoLi);
+    todoLi.appendChild(todoEditInput);
+    todoLi.appendChild(todoSpan);
+    todoLi.appendChild(editButton);
+    todoLi.appendChild(deleteButton);
+    newTodo.value = "";
+    saveList();
 }
 
-function editTodo(todo) {
-  todo.textContent = 'Done';
-  var thisLi = todo.parentNode;
+function editTodo(thisEditButton) {
+  thisEditButton.textContent = 'Done';
+  var thisLi = thisEditButton.parentNode;
   var thisTodoEditInput = thisLi.firstChild;
-  thisTodoEditInput.style.display = 'inline'
+  thisTodoEditInput.style.display = 'inline';
   thisSpan = thisTodoEditInput.nextSibling;
-  thisSpan.style.display = 'none'
+  thisSpan.style.display = 'none';
   thisTodoEditInput.value = thisSpan.textContent;
+  thisEditButton.setAttribute('onclick', 'editComplete(this)');
+  // runnung saveList() has no effect
+}
+
+function editComplete(thisEditButton) {
+  thisEditButton.textContent = "Edit";
+  var thisLi = thisEditButton.parentNode;
+  var thisTodoEditInput = thisLi.firstChild;
+  thisSpan = thisTodoEditInput.nextSibling;
+  thisSpan.textContent = thisTodoEditInput.value;
+  thisSpan.style.display = 'inline';
+  thisTodoEditInput.style.display = 'none';
+  thisEditButton.setAttribute('onclick', 'editTodo(this)');
+  saveList();
 }
 
 function deleteTodo(todo) {
-  document.getElementById("todoUl").removeChild(todo.parentNode)
-  saveList()
+  document.getElementById("todoUl").removeChild(todo.parentNode);
+  saveList();
 }
 
 function strikeOut(todo) {
     if (todo.style.textDecoration === 'none') {
     todo.style.textDecoration = 'line-through yellow double';
     } else {
-    todo.style.textDecoration = 'none'
+    todo.style.textDecoration = 'none';
     }
-    saveList()
+    saveList();
 }
-
-
-
 
 function saveList() {
   window.localStorage.setItem('savedList', todoUl.innerHTML);
